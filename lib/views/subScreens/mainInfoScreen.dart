f2aef14c9601121d65a49fd7d0d59785400b6a89
pavanmsgit <pavanmsfreelance>
2023-05-20 04:03:33 +0530

    Intial Commit

--- lib/views/subScreens/mainInfoScreen.dart
+++ lib/views/subScreens/mainInfoScreen.dart
@@ -14,6 +14,7 @@ import 'package:garbagecollector/widgets/shimmer.dart';
 import 'package:intl/intl.dart';
 
 import '../../controllers/launch_controller.dart';
+import '../../controllers/location_controller.dart';
 import '../../widgets/addressBar.dart';
 import '../../widgets/posterWidget.dart';
 import 'itemComponent/createItem.dart';
@@ -34,6 +35,8 @@ class _SpacesScreenState extends State<SpacesScreen> {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       getUserData();
       authController.getUserProfile();
+      locationController.getLocationPermission();
+      locationController.getLatestAddress().whenComplete(() => true);
     });
     super.initState();
   }
